2d8f66e6568a0befd94db86f3c67dd90fa0bc6e4
Alexis Bonnet <alexis.bonnet357@outlook.com>
2024-10-26 22:16:57 +0200

    affichage de la carte

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
-const charger_fichier = (url) => {
-    fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Erreur HTTP, statut = ' + response.status)
-            }
-            return response.json()
-        })
-        .then(data => {
-            console.log(data); // Affiche les données JSON dans la console
-            // Traite les données comme tu le souhaites
-        })
-        .catch(error => {
-            console.error('Erreur lors du chargement du fichier JSON :', error)
-        })
-
+const charger_fichier = async (url) => {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Erreur HTTP, statut = ' + response.status)
+        }
+        return await response.json()
+    } catch (error) {
+        console.error('Erreur lors du chargement du fichier JSON :', error)
+    }
 }
 
-const charger_carte = (nom) => {
-    carte = charger_fichier(`cartes/${nom}.json`)
+const charger_carte = async (nom) => {
+    carte = await charger_fichier(`cartes/${nom}.json`)
+    afficher_carte(carte)
+}
+
+const afficher_carte = (carte) => {
+    const mapContainer = document.querySelector(`main`);
+
+    carte.terrain.forEach((ligne, y) => {
+        const ligne_html = document.createElement('div')
+        ligne_html.classList.add('ligne')
+    
+        ligne.forEach((id_case, x) => {
+            const donnees_bloc = carte.cases[id_case]
+            const case_html = document.createElement('div')
+    
+            case_html.classList.add('case')
+            case_html.style.backgroundImage = `url(textures/decors/${donnees_bloc.texture}.png)`
+            if (x === carte.depart.x && y === carte.depart.y) {
+                case_html.classList.add('depart')
+            }
+            ligne_html.appendChild(case_html)
+        })
+        mapContainer.appendChild(ligne_html)
+    })
 }
 
 charger_carte("1_1")
